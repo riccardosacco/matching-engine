d847a24a933e5b14d7456fe84ddacafac0f636e7
rickybag <rickybag99@gmail.com>
2020-10-28 12:30:10 +0100

    added code for layers

--- layers/zip_layers.sh
+++ layers/zip_layers.sh
@@ -1,3 +1,7 @@
 cd dependencies && zip -r ../zip/dependencies.zip python
 
-cd ../matching_libs && zip -r ../zip/matching_libs.zip python
+cd ../query_management && zip -r ../zip/query_management.zip python
+
+cd ../elasticsearch_metadata && zip -r ../zip/elasticsearch_metadata.zip python
+
+cd ../alias_management && zip -r ../zip/alias_management.zip python
