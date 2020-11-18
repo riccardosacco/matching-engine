ef2af1732ca8baf91ca74bfedc7357ae53fbb892
rickybag <rickybag99@gmail.com>
2020-11-18 10:16:29 +0100

    query_management

--- layers/query_management/python/query_management/item_series.py
+++ layers/query_management/python/query_management/item_series.py
@@ -2,29 +2,23 @@ import json
 
 from query_management.query_fields.providerID import generate_providerID
 from query_management.query_fields.title import generate_title
-from query_management.query_fields.director import generate_director
-from query_management.query_fields.year import generate_year
 
 def generate_query(metadata):
     """
     Generate ElasticSearch query in JSON format
     Usage:
-        >>> from query_management import item_programme
-        >>> item_programme.generate_query(metadata)
+        >>> from query_management import item_series
+        >>> item_series.generate_query(metadata)
     Params:
         - metadata: {
             query_providerID: string,
-            query_titles: array[string],
-            query_directors: array[string],
-            query_year: string
+            query_titles: array[string]
         }
     Return: Query Object (in JSON format)
     """
 
     query_providerID = metadata.get("query_providerID")
     query_titles = metadata.get("query_titles")
-    query_directors = metadata.get("query_directors")
-    query_year = metadata.get("query_year")
 
     json_provider_query = {
         "query": {
@@ -61,20 +55,8 @@ def generate_query(metadata):
         nested_title = generate_title(query_titles, scores)
         json_metadata_query["query"]["bool"]["should"].append(nested_title)
 
-    if query_directors:
-        scores = {
-            "director_InitialsFuzzy": "20",
-            "director_ORFuzzy": "25"
-        }
 
-        nested_director = generate_director(query_directors, scores)
-        json_metadata_query["query"]["bool"]["should"].append(nested_director)
-
-    if query_year:
-        nested_year = generate_year(query_year)
-        json_metadata_query["query"]["bool"]["should"].append(nested_year)
-
-    if query_titles or query_directors or query_year:
+    if query_titles:
         json_output_query += "{}\n" + json.dumps(json_metadata_query)
 
     return json_output_query
