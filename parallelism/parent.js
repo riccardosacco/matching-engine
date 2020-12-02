19a0641c0d44a981ae76b07694c95e00f9e812af
rickybag <rickybag99@gmail.com>
2020-12-02 10:20:22 +0100

    added parallelism test

---
+++ parallelism/parent.js
@@ -0,0 +1,18 @@
+var child_process = require("child_process");
+
+var os = require("os");
+
+var numchild = os.cpus().length;
+var done = 0;
+
+for (var i = 0; i < numchild; i++) {
+  var child = child_process.fork("./child");
+  child.send((i + 1) * 1000);
+  child.on("message", function (message) {
+    console.log("[parent] received message from child:", message);
+    done++;
+    if (done === numchild) {
+      console.log("[parent] received all results");
+    }
+  });
+}
