5825f1eecb5db6deabb28e55aa48377bc58cf628
Riccardo Sacco <rickybag99@gmail.com>
2021-01-21 21:46:46 +0100

    added is_enriched & is_master

--- matching_engine/alias_management/__init__.py
+++ matching_engine/alias_management/__init__.py
@@ -1,45 +1,54 @@
 from matching_engine.elasticsearch_metadata import ElasticSearch
 
+
 class AliasManagement(ElasticSearch):
-  def __init__(self, url, index):
-    super().__init__(url, index)
+    def __init__(self, url, index):
+        super().__init__(url, index)
 
-  def create_alias(self, masterUUID, aliasUUID):
-    # Search aliasUUID and get metadata array
-    document = self.get_document_by_UUID(aliasUUID)
+    def create_alias(self, masterUUID, aliasUUID):
+        # Search aliasUUID and get metadata array
+        document = self.get_document_by_UUID(aliasUUID)
 
-    if document:
-      metadata_array = document["providerData"]
+        if document:
+            metadata_array = document["providerData"]
 
-      # For each metadata
-      for metadata in metadata_array:
-      # Add metadata to masterUUID
-        self.add_metadata_by_UUID(masterUUID, metadata)
+            # For each metadata
+            for metadata in metadata_array:
+                # Add metadata to masterUUID
+                self.add_metadata_by_UUID(masterUUID, metadata)
 
-      # Remove metadata from aliasUUID
-        self.delete_metadata_by_UUID(aliasUUID, metadata["UUID"])
-      return True
-    else:
-      return False
+            # Remove metadata from aliasUUID
+                self.delete_metadata_by_UUID(aliasUUID, metadata["UUID"])
 
-  def unalias(self, masterUUID, aliasUUID):
-    # Search masterUUID
-    document = self.get_document_by_UUID(masterUUID)
+            # Add is_master flag to masterUUID document
+            self.update_document_by_UUID(masterUUID, {"is_master": True})
 
-    if document:
-      metadata_array = document["providerData"]
+            return True
+        else:
+            return False
 
-    # Find aliasUUID in metadata array
-      metadata_array = [metadata for metadata in metadata_array if metadata["UUID"] == aliasUUID]
+    def unalias(self, masterUUID, aliasUUID):
+        # Search masterUUID
+        document = self.get_document_by_UUID(masterUUID)
 
-    # For each metadata
-      for metadata in metadata_array:
-        # Add metadata to aliasUUID
-        self.add_metadata_by_UUID(aliasUUID, metadata)
+        if document:
+            metadata_array = document["providerData"]
 
-        # Remove metadata from masterUUID
-        self.delete_metadata_by_UUID(masterUUID, metadata["UUID"])
+        # Find aliasUUID in metadata array
+            metadata_array = [
+                metadata for metadata in metadata_array if metadata["UUID"] == aliasUUID]
 
-      return True
-    else:
-      return False
+        # For each metadata
+            for metadata in metadata_array:
+                # Add metadata to aliasUUID
+                self.add_metadata_by_UUID(aliasUUID, metadata)
+
+                # Remove metadata from masterUUID
+                self.delete_metadata_by_UUID(masterUUID, metadata["UUID"])
+
+            # Remove is_master flag to masterUUID document
+            self.update_document_by_UUID(masterUUID, {"is_master": False})
+
+            return True
+        else:
+            return False
