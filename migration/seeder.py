fcbb4aedd7d172504cddadcc101caa20d6187920
Riccardo Sacco <rickybag99@gmail.com>
2021-01-27 15:45:43 +0100

    updated seeder

--- migration/seeder.py
+++ migration/seeder.py
@@ -4,9 +4,10 @@ from datetime import datetime
 fileinput = "input/program.csv"
 fileoutput = "data.json"
 
+# 1. Read CSV file and create DataFrame
 df = pd.read_csv(fileinput, delimiter=";", keep_default_na=False)
 
-
+# 2. Rename columns
 df.rename(
     columns={
         "date_time": "lastUpdate",
@@ -31,10 +32,11 @@ df.rename(
     inplace=True,
 )
 
+# 3. Parse data with functions
 df["lastUpdate"] = df["lastUpdate"].apply(
     lambda x: datetime.strptime(x, "%d/%m/%Y %H:%M")
 )
-df["alt_title"] = df["alt_title"].apply(lambda x: x.split("[[ALT]]")[1:])
+df["alt_title"] = df["alt_title"].apply(lambda x: list(set(x.split("[[ALT]]")[1:])))
 df["director"] = df["director"].apply(lambda x: x.split(",") if x != "" else "")
 df["entitySubtype"] = df["entitySubtype"].apply(
     lambda x: "episode" if x == "t" else "programme"
@@ -42,5 +44,5 @@ df["entitySubtype"] = df["entitySubtype"].apply(
 df["type"] = df["type"].apply(
     lambda x: "SCHED" if x in ["IBMS", "SCHED", "FINALIZED"] else "ENRICH"
 )
-
+# 4. Export to JSON with orient = "records"
 df.to_json(fileoutput, orient="records")
